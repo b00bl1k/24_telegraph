78a85c019dc8f7fbe88cf458536aeeaea12878da
Alexey Ryabov <6006l1k@gmail.com>
2019-03-22 23:21:46 +0300

    Fix host

--- server.py
+++ server.py
@@ -10,5 +10,5 @@ def form():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", debug=True, port=port)
